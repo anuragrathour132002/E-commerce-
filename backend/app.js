84c27af0358e8dd4760d669ae3b3ace5a21673f9
Anurag Rathour <anuragrathour47@gmail.com>
2023-11-25 18:43:31 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,9 @@ const express = require('express');
 const app = express();
 app.use(express.json())
 
-const producr = require("./routes/productRoutes");
+const product = require("./routes/productRoutes");
 
 app.use("/api/v1", product);
-
-
 
 
 module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const app = require("./app");
-const env = require("dotenv");
+const dotenv = require("dotenv");
 
 dotenv.config = ({ path: "backend/config/config.env" })
 app.listen(process.env.PORT, () => {
 
 
-    console.log(`the server is working on port http://localhost:$(process.env.PORT)`)
+    console.log(`the server is working on port http://localhost:${process.env.PORT}`)
 })
