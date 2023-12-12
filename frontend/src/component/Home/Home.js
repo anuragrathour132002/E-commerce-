0025054194e076863c7753bb781557ba25c9ceba
anuragrathour132002 <anuragrathour47@gmail.com>
2023-12-12 23:13:46 +0530

    add redux

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,32 +1,46 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { gMouse } from "react-icons/all";
 import ".Home.css";
-import product from "./Product.js"
+import Products from "./Product.js"
+import Metadata from "./component/layout/Metadata.js";
+import { getProduct } from "../../actions/productAction.js";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../layout/Loader/Loader.js"
 
-const product = {
-    name: "Blur shirt",
-    images: [{ url: "fjf" }],
-    price: "5000",
-    _id: "anu",
-};
 const Home = () => {
+
+    const dispatch = useDispatch();
+    const { loading, error, products, productsCount } = useSelector(
+        (state) => state.products
+    );
+
+    useEffect(() => {
+        dispatch(getProduct());
+    }, [dispatch]);
     return (
         <Fragment>
-            <div class="banner">
-                <p>Welcome to ecommerce</p>
-                <h1>Find Amazing Product</h1>
+            {loading ? <Loader /> : (
+                <Fragment>
+                    <Metadata title="ECommerce" />
+                    <div class="banner">
+                        <p>Welcome to ecommerce</p>
+                        <h1>Find Amazing Product</h1>
 
-                <a href="#container">
-                    <button>
-                        Scroll <gMouse />
-                    </button>
-                </a>
-            </div>
-            <h2 className='homeHeading'>Featured Products</h2>
-            <div className='container' id='cosntainer'>
-                <Product product={product} />
-            </div>
+                        <a href="#container">
+                            <button>
+                                Scroll <gMouse />
+                            </button>
+                        </a>
+                    </div>
+                    <h2 className='homeHeading'>Featured Products</h2>
+                    <div className='container' id='cosntainer'>
+                        {products && products.map(product => (
+                            <Products product={product} />
+                        ))}
+                    </div>
 
+                </Fragment>
+            )}
         </Fragment>
     )
 }
---
+++ frontend/src/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-dectools-extension";
+import { productReducer } from "./reducers/productReducers";
+
+const reducer = combineReducers({
+    products: productReducer,
+});
+
+let initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
